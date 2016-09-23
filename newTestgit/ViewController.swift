517c6ff8522b8d08576d6f2e4d387e1fd8ad73ca
Osama Jassim <Osama@Osamas-MacBook-Pro.local>
2016-09-24 00:18:13 +0300

    done changing that thing here

--- newTestgit/ViewController.swift
+++ newTestgit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 view.backgroundColor = UIColor.red
+        print("hello world")
     }
 
     override func didReceiveMemoryWarning() {
